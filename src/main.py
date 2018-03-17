32a33f8db87d4dd45aaa91121e6da2c20e618762
devilwarz <pallabmaji@hotmail.com>
2018-03-17 22:12:30 +0530

    Added:Feature training - package added

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+Project Information:
+    Author      : Pallab Maji
+    Co-Authors  : Rahul Ambati
+    Date        : 
+    Project     : PoesNet - Pose Estimation Network
+    
+Functional Information:
+    Aim     : To detect joint points and in turn generate skeleton for all persons present in the image
+    Inputs  :
+    Outputs :
+
+"""
+
+import keras
+
